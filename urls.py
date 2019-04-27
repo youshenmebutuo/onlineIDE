88acd747ab977c82cd1a0d725df4f1cc150f7301
yc <1151727370@qq.com>
2019-04-27 14:55:51 +0800

    增加xterm.js组件库

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from handlers.main import MainHandler
 from handlers.websocket import WebSocketHandler
 from handlers.fileupload import FileUploadHandler
+from terminado import TermSocket, SingleTermManager
 
 handlers = [
     (r"/", MainHandler),
     (r'/websocket', WebSocketHandler),
     (r"/file/upload", FileUploadHandler),
+    (r"/terminal", TermSocket, {'term_manager': SingleTermManager(shell_command=['bash'])})
 ]
